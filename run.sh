f361bcaf1ee71163c426de05d0ea0eed0e0adfe4
flecke <57179324+flecke-m@users.noreply.github.com>
2025-04-04 14:48:05 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 
 echo "[INFO] Preparing to start...checking for devices.json"
 
-if ! test -f /data/devices.json; then
+if ! test -f /open3e/devices.json; then
     # Read the config file
-    config_file="/data/args"
+    config_file="/open3e/args"
     while IFS= read -r line; do
         case "$line" in
             --can)
@@ -18,10 +18,10 @@ if ! test -f /data/devices.json; then
     done < "$config_file"
 
     echo "[INFO] Running open3e_depictSystem -c $CAN ... This may take a while"
-    cd /data
+    cd /open3e
     open3e_depictSystem -c $CAN
 fi
 
-cd /data
+cd /open3e
 echo "[INFO] Starting Open3e... "
 open3e @args
